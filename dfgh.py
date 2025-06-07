052671861999ee4ab7b632357e7f423824b7a11c
Juan David Cadena <jcbprofesion@gmail.com>
2025-06-07 17:37:38 -0500

    trabajo pendiente

---
+++ dfgh.py
@@ -0,0 +1,14 @@
+# ju = (10 / 100) * 100
+# print (ju)
+# f = int(input("ingresa un numero "))
+# n = (5 * (f // 5)) - (3*(f // 3))
+# print (n)
+
+
+nombres = [(input("ingresa un nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre "))]
+print (nombres)
+nombres2 = (nombres.upper())
+if nombres2 == "MARIA":
+    print("nombre correcto")
+else :
+    print("nombre diferente ")
--- taller de diagramas de flujo.py
+++ taller de diagramas de flujo.py
@@ -36,7 +36,7 @@
 # la_mari = [int(input("ingresa el precio del primer producto")),int(input("ingresa el precio del segundo producto")),int(input("ingresa el precio del tercer producto")),]
 # la_marit = la_mari[0] + la_mari[1] + la_mari[2]
 # if la_marit < 100:
-#     print(f"el precio de sus productos con descuento incluido es de {la_mari - 10%}")
+#     print(f"el precio de sus productos con descuento incluido es de {((10 / 100) * la_mari) - la_mari}")
 # else:
 #     print(f"el precio de su producto es {la_mari}")
     
@@ -47,10 +47,87 @@
 # else :
 #     print("usted no puede votar")
 #8 
-cliente = [float(input("ingrese el total de sus compras")),input("usted es vip, ¿si o no?").upper]
-if cliente[1] == "SI":
-    print(f"su total es de {cliente[0] - 20} que tenga un buen dia")
+# dinero = float(input("ingrese el total de su compra "))
+# cliente = input("usted es vip, ¿si o no? ")
+# j = cliente.upper()
+# if j == "SI":
+#     print(f"su total es de {dinero - ((20 / 100) * dinero) } que tenga un buen dia")
+# else :
+#     print(f"su total es {dinero} que tenga un buen dia")
+# #9
+# f = int(input("ingresa un numero "))
+# n = (5 * (f // 5)) - (3*(f // 3))
+# if n == 0:
+#     print(f"el numero {f} es multiplo de 3 y 5")
+# else :
+#     print("el numero ingresado no es multiplo de 3 y 5")
+#10 
+# numero_dado = int(input("ingresa un numero "))
+# verifi = int(input("ingrese el numero ha verificar si es divisible "))
+# verifi2 = int(input("ingrese el segundo numero ha verificar si es divisible "))
+# operacion7 = (verifi * (numero_dado // verifi)) - (verifi2*(numero_dado // verifi2))
+# if operacion7 == 0:
+#     print(f"el numero {numero_dado} es divisible entre {verifi} y {verifi2}")
+# else :
+#     print(f"el numero {numero_dado} no es divisible entre {verifi} y {verifi2}")
+#11
+# la_lista = [int(input("ingresa un numero ")),int(input("ingresa otro numero ")),int(input("ingresa otro numero ")),int(input("ingresa otro numero ")),int(input("ingresa otro numero "))]
+# if la_lista[2] > 10 :
+#     print("es mayor a 10")
+# else :
+#     print("no es mayor a 10")
+# #12
+# if la_lista[0] == 7:
+#     print("el 7 sa halla en la lista")
+# elif la_lista[1] == 7:
+#     print("el 7 sa halla en la lista")
+# elif la_lista[2] == 7:
+#     print("el 7 sa halla en la lista")
+# elif la_lista[3] == 7:
+#     print("el 7 sa halla en la lista")
+# elif la_lista[4] == 7:
+#     print("el 7 sa halla en la lista")
+# else:
+#     print("el 7 no se halla en la lista")
+# #13
+# suma_lista = la_lista[0] + la_lista[1]
+# if suma_lista > 10:
+#     print("la suma es alta")
+# else :
+#     print("la resta es menor ")
+# #14
+# nombres = [(input("ingresa un nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre ")),(input("ingresa otro nombre "))]
+# print (nombres)
+# if nombres[-1] == "maria":
+#     print("nombre correcto")
+# else :
+#     print("nombre diferente ")
+# #15
+# sonic_colors = [(input("ingresa un color ")),(input("ingresa otro color ")),(input("ingresa otro color "))]
+# if sonic_colors[1]  == "azul":
+#     sonic_colors[1] = "verde"
+# else :
+#     print(sonic_colors)
+# #16
+t =  (int(input("ingrese un numero ")),int(input("ingrese un numero ")),int(input("ingrese un numero ")),int(input("ingrese un numero ")),int(input("ingrese un numero ")),)
+if t[0] > t[-1]:
+    print("lista en orden hacendente")
+else:
+    print("lista decendente ")
+#17
+if t[1] > 30 :
+    prit("edad mayor a treinta")
 else :
-    print(f"su total es {cliente} que tenga un buen dia")
-#9
-# nume4 = int(input("ingrese el num"))
+    print("edad menor o igual a 30")
+#18
+tu = list(t)
+if tu[1] == 2:
+    tu[1] = 10
+hol = tuple(tu)
+print(hol)
+#19
+if hol[1] > 5 :
+    print("cordenada alta")
+else :
+    print("cordenada baja")
+#20
